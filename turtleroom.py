c378ec1e95a5eda33b2bb82ce027a1c644583ded
adijunn <adivganapathi@gmail.com>
2014-04-26 14:15:02 -0700

    pythonroom update

--- turtleroom.py
+++ turtleroom.py
@@ -1,18 +1,22 @@
 # author: adijunn
 import turtle
 
-length = 100
+sides = 6
+#count = 0;
+length = 60
+angle = 180 - 180*(sides - 2) / sides
+numbers = range(0, sides)
 adi = turtle.Turtle()
 
-adi.forward(length)
-adi.left(60)
-adi.forward(length)
-adi.left(60)
-adi.forward(length)
-adi.left(60)
-adi.forward(length)
-adi.left(60)
-adi.forward(length)
-adi.left(60)
-adi.forward(length)
-adi.left(60)
+#while count < 10: 
+
+	#adi.forward(length)
+	#adi.left(36)
+	#count = count + 1
+	
+	
+for number in numbers: 
+	
+	adi.forward(length)
+	adi.left(angle)
+	
