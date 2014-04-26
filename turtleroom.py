9487852c1acdb859712ae68130480e1504d37377
adijunn <adivganapathi@gmail.com>
2014-04-26 13:19:05 -0700

    pythonroom update

--- turtleroom.py
+++ turtleroom.py
@@ -1,1 +1,18 @@
 # author: adijunn
+import turtle
+
+length = 100
+adi = turtle.Turtle()
+
+adi.forward(length)
+adi.left(60)
+adi.forward(length)
+adi.left(60)
+adi.forward(length)
+adi.left(60)
+adi.forward(length)
+adi.left(60)
+adi.forward(length)
+adi.left(60)
+adi.forward(length)
+adi.left(60)
